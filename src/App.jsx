434478c1723068c25ddd85cfa42eb180ab3f10b5
NPK13 <nishalbabupk@gmail.com>
2023-05-25 16:32:51 +0530

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,26 @@
+import Navbar from './components/Navbar';
+import { Routes, Route } from 'react-router-dom';
+import Home from './pages/home';
+import About from './pages/about';
+
+
+
+
+
+function App() {
+  return (
+    <div>
+
+<>
+<Navbar />
+       <Routes>
+          <Route path="/" element={<Home />} />
+        
+          <Route path="/about" element={<About />} />
+       </Routes>
+    </>
+    </div>
+  );
+}
+
+export default App;
---
+++ src/pages/home.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+export default function Home() {
+  return (
+    <div>
+        <div>
+    <img style={{height:'850px',width:'100%'}} src="https://images.pexels.com/photos/268533/pexels-photo-268533.jpeg?cs=srgb&dl=pexels-pixabay-268533.jpg&fm=jpg" alt="" />
+    </div>
+    </div>
+  )
+}
